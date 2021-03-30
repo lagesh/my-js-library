caef5a2d3fcb05e0656c985c309e2620983e60ab
Olu Olageshin <44141085+lagesh@users.noreply.github.com>
2021-03-30 19:47:52 +0100

    add JSX basic syntax samples

---
+++ react.js
@@ -0,0 +1,8 @@
+// ============ JSX syntax ==============
+/*
+JSX is very much like Html in Js but with slight differences. This line below will pass as JSX
+const heading = <h1>I am a heading</h1>; 
+(don't forget to end with the semi-colon as in normal Js, since it's more or less a Js decalaration).
+
+If you have a long line of JSX, it's best to break it down for readability
+*/
